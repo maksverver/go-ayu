b8ee57d3384006c3020ebb3181a36dc6dcb04f36
Maks Verver <maksverver@geocities.com>
2014-11-03 21:40:36 +0100

    Fix duplicate encoding of player keys in game URLs.

--- server/static/create.js
+++ server/static/create.js
@@ -13,10 +13,8 @@
 				var a = document.getElementById('link-' + i)
 				a.target = 'game-' + res.game + '_' + i
 				a.href = 'game.html#game=' + encodeURIComponent(res.game)
-				for (var j = 0; j < 2; ++j) {
-					if (i&1) a.href += '&white=' + encodeURIComponent(res.keys[0])
-					if (i&2) a.href += '&black=' + encodeURIComponent(res.keys[1])
-				}
+				if (i&1) a.href += '&white=' + encodeURIComponent(res.keys[0])
+				if (i&2) a.href += '&black=' + encodeURIComponent(res.keys[1])
 			}
 		}
 	}
